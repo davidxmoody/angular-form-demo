c8c3db199baec5dc3a8df5f258ce993db6dc70c9
David Moody <david@davidxmoody.com>
2015-02-28 15:27:52 +0000

    Improved CSS for mobile viewing

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 var app = angular.module('formApp', []);
 
-app.controller('FormCtrl', function($scope) {
+app.controller('FormCtrl', function($scope, $window) {
 
   $scope.titles = [
     'Mr',
@@ -9,17 +9,25 @@ app.controller('FormCtrl', function($scope) {
     // Could add more, e.g. "Miss", "Dr", "Prof", etc.
   ]
 
-  $scope.user = {};
+  // Fill in the current time to the nearest minute
+  var now = new Date();
+  now.setSeconds(0);
+  now.setMilliseconds(0);
+
+  $scope.user = {
+    currentDatetime: now
+  };
 
   $scope.stage = 1;
 
   $scope.nextStage = function() {
     $scope.stage++;
+    $window.scrollTo(0, 0);
   };
 
   $scope.submit = function() {
     console.log($scope.user);
-    $scope.stage++;
+    $scope.nextStage();
   };
 
   $scope.getUserData = function() {
