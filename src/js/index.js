50f9fe30816c4299cf56f4067717bea83fe5b35e
David Moody <david@davidxmoody.com>
2015-02-28 11:50:04 +0000

    Moved available titles from HTML to script

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,24 @@
 var app = angular.module('formApp', []);
 
 app.controller('FormCtrl', function($scope) {
+
+  $scope.titles = [
+    'Mr',
+    'Ms',
+    'Mrs'
+    // Could add more, e.g. "Miss", "Dr", "Prof", etc.
+  ]
+
   $scope.user = {};
+
   $scope.stage = 1;
+
   $scope.nextStage = function() {
     $scope.stage++;
   };
+
   $scope.submit = function() {
     console.log($scope.user);
   };
+
 });
