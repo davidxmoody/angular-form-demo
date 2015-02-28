3ca2dbcd2bad2797f557e346eccf3ef8fa44e484
David Moody <david@davidxmoody.com>
2015-02-28 11:14:59 +0000

    Gulp now watches for changes and serves files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 
-gulp.task('default', ['html', 'css', 'js']);
+gulp.task('default', ['html', 'css', 'js', 'watch', 'connect']);
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build'
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.html', ['html']);
+  gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/css/**/*.scss', ['css']);
+});
 
 gulp.task('html', function() {
   gulp.src('src/**/*.html')
