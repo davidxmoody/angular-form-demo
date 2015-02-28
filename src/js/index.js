2f8f607c519e3d03a5af2de387012b370c060c98
David Moody <david@davidxmoody.com>
2015-02-28 18:14:17 +0000

    Added better comments to html and js files

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,29 @@
 var app = angular.module('formApp', ['ngAnimate']);
 
-app.controller('FormCtrl', function($scope, $http, $window) {
+app.controller('FormCtrl', function($scope, $http) {
 
   $scope.requestBinURL = 'http://requestb.in/1jz6x6q1';
 
+  // List of selectable titles
   $scope.titles = [
     'Mr',
     'Ms',
     'Mrs'
-    // Could add more, e.g. "Miss", "Dr", "Prof", etc.
   ]
 
+  // Storage for all submitted data
   $scope.user = {};
 
+  // This stage number controls the active ng-switch element in index.html
   $scope.stage = 1;
 
+  // Called upon submitting the first two stages of the form
   $scope.nextStage = function() {
     $scope.stage++;
-    $window.scrollTo(0, 0);
+
+    // If advancing to stage 2 then pre-fill the current datetime, saves the 
+    // user from having to enter a new datetime if the current datetime is okay
     if ($scope.stage===2 && !$scope.user.currentDatetime) {
-      // Fill in the current time to the nearest minute
       var now = new Date();
       now.setSeconds(0);
       now.setMilliseconds(0);
@@ -27,12 +31,17 @@ app.controller('FormCtrl', function($scope, $http, $window) {
     }
   };
 
+  // Called upon submitting form stage 3, sends data to requestb.in and
+  // displays confirmation message with JSON data
   $scope.submit = function() {
     $scope.nextStage();
     console.log($scope.user);
 
     // Needs to be sent with the following Content-Type to be picked up by
     // requestb.in properly
+    // The response cannot be read because requestb.in does not send an
+    // Access-Control-Allow-Origin header, this is not a problem as the data
+    // is still recieved by requestb.in without issue
     $http({
       method: 'post',
       url: $scope.requestBinURL,
@@ -41,6 +50,7 @@ app.controller('FormCtrl', function($scope, $http, $window) {
     });
   };
 
+  // Convenience function to pretty print JSON output in the confirmation stage
   $scope.getUserData = function() {
     return JSON.stringify($scope.user, null, 2);
   };
