d490a8db8a0ad65cbc4ecc36d3a040126362c59e
David Moody <david@davidxmoody.com>
2015-02-28 11:44:31 +0000

    Added stages two and three

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 var app = angular.module('formApp', []);
 
 app.controller('FormCtrl', function($scope) {
+  $scope.user = {};
   $scope.stage = 1;
   $scope.nextStage = function() {
     $scope.stage++;
+  };
+  $scope.submit = function() {
+    console.log($scope.user);
   };
 });
